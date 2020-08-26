3083618a9c04ac49344e29964ca24a3a9525af1d
Gabriel Asorey <gasorey@gmail.com>
2020-08-26 07:01:07 -0300

    failure: not completed SWAPI is down

--- 04-lists/for-forin/1.for-forin-forof.js
+++ 04-lists/for-forin/1.for-forin-forof.js
@@ -4,12 +4,26 @@ async function main(){
   try{
     const result = await service.getPeople('a')
     const names = []
-    console.time('for')
-    for (let i = 0; i<= result.results.length -1; i++){
-      const people = result.results[i]
+    //for
+
+    // console.time('for')
+    // for (let i = 0; i<= result.results.length -1; i++){
+    //   const people = result.results[i]
+    //   names.push(people.name)
+    // }
+    // console.timeEnd('for')
+
+    //forIn
+    // The difference between each for is inside the condition
+
+    // for(let i in result.results){
+    //   const people = result.results[i]
+    //   names.push(people.name)
+    // }
+
+    for(people of result.results){
       names.push(people.name)
     }
-    console.timeEnd('for')
   }
   catch(err){
     console.error('error',err)
