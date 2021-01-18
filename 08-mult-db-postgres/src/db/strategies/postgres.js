11604fa429cef73d52d2cd103cd6667e7d56f684
Gabriel <gasorey@gmail.com>
2021-01-18 09:09:55 -0300

    feat: module 8 add postgres db

---
+++ 08-mult-db-postgres/src/db/strategies/postgres.js
@@ -0,0 +1,62 @@
+const ICrud = require('./interfaces/ICrud')
+const Sequelize = require('sequelize')
+
+
+
+class Postgres extends ICrud {
+  constructor(){
+    super()
+    this.driver = null
+    this._herois = null
+    this._connect()
+  }
+  async isConnected(){
+    try{
+      await this._driver.authenticate()
+      return true
+    }catch(err){
+      console.log('err',err)
+    }
+  }
+ async defineModel(){
+   this._herois = driver.define('herois', {
+      id: {
+        type: Sequelize.INTEGER,
+        required: true,
+        primaryKey: true,
+        autoIncrement: true
+      },
+      nome: {
+        type: Sequelize.STRING,
+        required: true
+      },
+      poder: {
+        type: Sequelize.STRING,
+        required: true
+      }
+    },{
+      tableName: 'TB_HEROIS',
+      freezeTableName: false,
+      timestamps: false
+    })
+    await Herois.sync()
+  }
+  _connect(){
+     this._driver = new Sequelize(
+      'heroes',
+      'gasorey',
+      'docker',
+      {
+        host: 'localhost',
+        dialect: 'postgres',
+        quoteIdentifiers: false,
+        operatorAliases: false
+      }
+    )
+  }
+  create(item){
+    console.log('O item foi salvo no postgres')
+  }
+}
+
+module.exports = Postgres
